20d93a7774cd90fd4bd500b087bab0d02d638e51
hajizadeh <narges.hajizadeh65@gmail.com>
2025-07-09 18:10:58 +0330

    debuged database

--- src/main/java/salary/model/repository/sql/database.sql
+++ src/main/java/salary/model/repository/sql/database.sql
@@ -100,8 +100,8 @@ create sequence loan_items_seq start with 1 increment by 1;
 CREATE OR REPLACE VIEW SalaryReportNet AS
 WITH base_data AS (
     SELECT
-        Pay.id                                                                AS Payslips_id,
-        u.user_name                                                           AS users_user_name,
+        Pay.id AS Payslips_id,
+        u.user_name AS users_user_name,
         e.first_Name,
         e.last_Name,
         e.national_Id,
@@ -114,30 +114,29 @@ WITH base_data AS (
         e.position,
         e.hire_Date,
         e.termination_Date,
-        wr.days_worked * e.daily_salary                                       AS base_salary,
-        wr.over_time_Hours * (e.daily_salary / 8 * 1.4)                       AS over_time,
 
-        NVL(e.number_of_children, 0) * (al.child_allowance / 30) * wr.days_worked AS child_allowance,
-        CASE WHEN e.married = 1 THEN (al.marriage_allowance / 30) * wr.days_worked ELSE 0 END AS marriage_allowance,
-        (al.housing_allowance / 30) * wr.days_worked                          AS housing_allowance,
-        (al.food_allowance / 30) * wr.days_worked                             AS food_allowance,
+        wr.days_worked * NVL(e.daily_salary, 0) AS base_salary,
+        wr.over_time_Hours * (NVL(e.daily_salary, 0) / 8 * 1.4) AS over_time,
 
+        NVL(e.number_of_children, 0) * (NVL(al.child_allowance, 0) / 30) * wr.days_worked AS child_allowance,
+        CASE WHEN NVL(e.married, 0) = 1 THEN (NVL(al.marriage_allowance, 0) / 30) * wr.days_worked ELSE 0 END AS marriage_allowance,
+        (NVL(al.housing_allowance, 0) / 30) * wr.days_worked AS housing_allowance,
+        (NVL(al.food_allowance, 0) / 30) * wr.days_worked AS food_allowance,
 
         (
-            wr.days_worked * e.daily_salary
-                + wr.over_time_Hours * (e.daily_Salary / 8 * 1.4)
-                + NVL(e.number_of_children, 0) * (al.child_allowance / 30) * wr.days_worked
-                + CASE WHEN e.married = 1 THEN (al.marriage_allowance / 30) * wr.days_worked ELSE 0 END
-                + (al.housing_allowance / 30) * wr.days_worked
-                + (al.food_allowance / 30) * wr.days_worked
-
-            )                                                                     AS gross_salary,
+            wr.days_worked * NVL(e.daily_salary, 0)
+                + wr.over_time_Hours * (NVL(e.daily_Salary, 0) / 8 * 1.4)
+                + NVL(e.number_of_children, 0) * (NVL(al.child_allowance, 0) / 30) * wr.days_worked
+                + CASE WHEN NVL(e.married, 0) = 1 THEN (NVL(al.marriage_allowance, 0) / 30) * wr.days_worked ELSE 0 END
+                + (NVL(al.housing_allowance, 0) / 30) * wr.days_worked
+                + (NVL(al.food_allowance, 0) / 30) * wr.days_worked
+            ) AS gross_salary,
 
         (SELECT NVL(SUM(li.amount_paid), 0)
          FROM loan_items li
-         WHERE li.payslip_id = pay.id)                                        AS loan_payment,
+         WHERE li.payslip_id = Pay.id) AS loan_payment,
 
-        wr.under_Time_Hours * (e.daily_Salary / 8)                            AS under_time_payment,
+        wr.under_Time_Hours * (NVL(e.daily_Salary, 0) / 8) AS under_time_payment,
 
         Pay.issueDate,
         Pay.period
@@ -145,90 +144,78 @@ WITH base_data AS (
              JOIN users u ON Pay.users_id = u.id
              JOIN employees e ON Pay.employees_id = e.id
              JOIN work_Records wr ON Pay.work_Records_id = wr.id
-             JOIN allowances al ON EXTRACT(YEAR FROM Pay.issueDate) = al.year AND al.active = 1
+             LEFT JOIN allowances al
+                       ON EXTRACT(YEAR FROM Pay.issueDate) = al.year AND al.active = 1
 )
 
--- ادامه محاسبات همانند قبل (gross_salary موجوده)
 SELECT bd.*,
 
        ROUND(bd.gross_salary * 0.07, 2) AS employee_ins,
        ROUND(bd.gross_salary * 0.23, 2) AS employer_ins,
 
+       -- مالیات ماهانه بر اساس پلکان‌ها
        ROUND(
                CASE
-                   WHEN bd.gross_salary <= 240000000.00 THEN 0
-                   WHEN bd.gross_salary <= 300000000.00 THEN (bd.gross_salary - 240000000.00) * 0.10
-                   WHEN bd.gross_salary <= 380000000.00 THEN (300000000.00 - 240000000.00) * 0.10
-                       + (bd.gross_salary - 300000000.00) * 0.15
-                   WHEN bd.gross_salary <= 500000000.00 THEN (300000000.00 - 240000000.00) * 0.10
-                       + (380000000.00 - 300000000.00) * 0.15
-                       + (bd.gross_salary - 380000000.00) * 0.20
-                   WHEN bd.gross_salary <= 667000000.00 THEN (300000000.00 - 240000000.00) * 0.10
-                       + (380000000.00 - 300000000.00) * 0.15
-                       + (500000000.00 - 380000000.00) * 0.20
-                       + (bd.gross_salary - 500000000.00) * 0.25
-                   ELSE (300000000.00 - 240000000.00) * 0.10
-                       + (380000000.00 - 300000000.00) * 0.15
-                       + (500000000.00 - 380000000.00) * 0.20
-                       + (667000000.00 - 500000000.00) * 0.25
-                       + (bd.gross_salary - 667000000.00) * 0.30
+                   WHEN bd.gross_salary <= 240000000 THEN 0
+                   WHEN bd.gross_salary <= 300000000 THEN (bd.gross_salary - 240000000) * 0.10
+                   WHEN bd.gross_salary <= 380000000 THEN (300000000 - 240000000) * 0.10
+                       + (bd.gross_salary - 300000000) * 0.15
+                   WHEN bd.gross_salary <= 500000000 THEN (300000000 - 240000000) * 0.10
+                       + (380000000 - 300000000) * 0.15
+                       + (bd.gross_salary - 380000000) * 0.20
+                   WHEN bd.gross_salary <= 667000000 THEN (300000000 - 240000000) * 0.10
+                       + (380000000 - 300000000) * 0.15
+                       + (500000000 - 380000000) * 0.20
+                       + (bd.gross_salary - 500000000) * 0.25
+                   ELSE (300000000 - 240000000) * 0.10
+                       + (380000000 - 300000000) * 0.15
+                       + (500000000 - 380000000) * 0.20
+                       + (667000000 - 500000000) * 0.25
+                       + (bd.gross_salary - 667000000) * 0.30
                    END, 0
        ) AS monthly_tax,
 
+       -- مجموع کسورات
        ROUND(
-               bd.gross_salary * 0.07
-                   + bd.gross_salary * 0.23
+               bd.gross_salary * 0.07 + bd.gross_salary * 0.23
                    + CASE
                          WHEN bd.gross_salary <= 240000000 THEN 0
                          WHEN bd.gross_salary <= 300000000 THEN (bd.gross_salary - 240000000) * 0.10
-                         WHEN bd.gross_salary <= 380000000 THEN (300000000 - 240000000) * 0.10
-                             + (bd.gross_salary - 300000000) * 0.15
+                         WHEN bd.gross_salary <= 380000000 THEN (300000000 - 240000000) * 0.10 + (bd.gross_salary - 300000000) * 0.15
                          WHEN bd.gross_salary <= 500000000 THEN (300000000 - 240000000) * 0.10
-                             + (380000000 - 300000000) * 0.15
-                             + (bd.gross_salary - 380000000) * 0.20
+                             + (380000000 - 300000000) * 0.15 + (bd.gross_salary - 380000000) * 0.20
                          WHEN bd.gross_salary <= 667000000 THEN (300000000 - 240000000) * 0.10
-                             + (380000000 - 300000000) * 0.15
-                             + (500000000 - 380000000) * 0.20
-                             + (bd.gross_salary - 500000000) * 0.25
+                             + (380000000 - 300000000) * 0.15 + (500000000 - 380000000) * 0.20 + (bd.gross_salary - 500000000) * 0.25
                          ELSE (300000000 - 240000000) * 0.10
-                             + (380000000 - 300000000) * 0.15
-                             + (500000000 - 380000000) * 0.20
-                             + (667000000 - 500000000) * 0.25
-                             + (bd.gross_salary - 667000000) * 0.30
+                             + (380000000 - 300000000) * 0.15 + (500000000 - 380000000) * 0.20
+                             + (667000000 - 500000000) * 0.25 + (bd.gross_salary - 667000000) * 0.30
                    END
-                   + bd.loan_payment
-                   + bd.under_time_payment
+                   + bd.loan_payment + bd.under_time_payment
            , 0) AS total_deductions,
 
+       -- حقوق خالص
        ROUND(
-               bd.gross_salary
-                   - (
-                   bd.gross_salary * 0.07
-                       + bd.gross_salary * 0.23
+               bd.gross_salary -
+               (
+                   bd.gross_salary * 0.07 + bd.gross_salary * 0.23
                        + CASE
                              WHEN bd.gross_salary <= 240000000 THEN 0
                              WHEN bd.gross_salary <= 300000000 THEN (bd.gross_salary - 240000000) * 0.10
-                             WHEN bd.gross_salary <= 380000000 THEN (300000000 - 240000000) * 0.10
-                                 + (bd.gross_salary - 300000000) * 0.15
+                             WHEN bd.gross_salary <= 380000000 THEN (300000000 - 240000000) * 0.10 + (bd.gross_salary - 300000000) * 0.15
                              WHEN bd.gross_salary <= 500000000 THEN (300000000 - 240000000) * 0.10
-                                 + (380000000 - 300000000) * 0.15
-                                 + (bd.gross_salary - 380000000) * 0.20
+                                 + (380000000 - 300000000) * 0.15 + (bd.gross_salary - 380000000) * 0.20
                              WHEN bd.gross_salary <= 667000000 THEN (300000000 - 240000000) * 0.10
-                                 + (380000000 - 300000000) * 0.15
-                                 + (500000000 - 380000000) * 0.20
-                                 + (bd.gross_salary - 500000000) * 0.25
+                                 + (380000000 - 300000000) * 0.15 + (500000000 - 380000000) * 0.20 + (bd.gross_salary - 500000000) * 0.25
                              ELSE (300000000 - 240000000) * 0.10
-                                 + (380000000 - 300000000) * 0.15
-                                 + (500000000 - 380000000) * 0.20
-                                 + (667000000 - 500000000) * 0.25
-                                 + (bd.gross_salary - 667000000) * 0.30
+                                 + (380000000 - 300000000) * 0.15 + (500000000 - 380000000) * 0.20
+                                 + (667000000 - 500000000) * 0.25 + (bd.gross_salary - 667000000) * 0.30
                        END
-                       + bd.loan_payment
-                       + bd.under_time_payment
+                       + bd.loan_payment + bd.under_time_payment
                    )
            , 0) AS total_salary
 
 FROM base_data bd;
+
 
 
 CREATE OR REPLACE VIEW EmployeeLoanReport AS
